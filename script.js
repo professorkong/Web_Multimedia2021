f49147b2197a947a91c93ea005f179d8adcb5270
professorkong <theoneofkong@gmail.com>
2022-05-26 04:05:49 +0700

    Add

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// No JS thanks to the Keith/Kellum technique
+function btn(){
+      let image = document.getElementById('image1');
+      let images = ['https://avatars.githubusercontent.com/u/81680997?s=400&u=ed7cae600405403aaf1c4f7120dc80c8a6b06ffe&v=4', 'https://media.discordapp.net/attachments/932970063718387763/979116417012072519/unknown.png?width=595&height=670',
+      'https://media.discordapp.net/attachments/932970063718387763/979116561967247400/unknown.png?width=658&height=671'];
+      let random = Math.floor(Math.random() * 3);
+      image.src = images[random];
+    }
